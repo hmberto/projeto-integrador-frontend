1c1e3c9b4f9a374e4172c4255113268af31b1a3c
Humberto <humbertoodantas@gmail.com>
2022-03-26 19:40:24 -0300

    Register model

---
+++ src/models/register.ts
@@ -0,0 +1,16 @@
+
+export class Register {
+    name: string;
+	email: string;
+	pass: string;
+	street: string;
+	number: string;
+	complement: string;
+	zipCode: string;
+	state: string;
+	city: string;
+	district: string;
+	cpf: string;
+	birthDate: string;
+	sex: string;
+}
