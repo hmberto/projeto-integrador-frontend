421af2a47ccaa8ffb6f5d52ab36a1d4d817bf880
Humberto <humbertoodantas@gmail.com>
2022-03-27 21:11:16 -0300

    Pharmacy fields

--- src/mock/pharmacy-mock.ts
+++ src/mock/pharmacy-mock.ts
@@ -1,8 +1,8 @@
 import { Pharmacy } from '../models/pharmacy';
 
 export const PHARMACY : Pharmacy[] = [
-    { name: 'Drogaria São Paulo', image: 'assets/pharmacies/drogaria_sp_logo.jpg' },
-    { name: 'Ultrafarma', image: 'assets/pharmacies/ultrafarma_logo.jpg' },
-    { name: 'Pague Menos', image: 'assets/pharmacies/pague_menos_logo.jpg' },
-    { name: 'Droga Raia', image: 'assets/pharmacies/droga_raia_logo.jpg' }
+    { name: 'Drogaria São Paulo', image: 'assets/pharmacies/drogaria_sp_logo.jpg', distance: '3.2 km', time: '50-60 min', tax: '8,99' },
+    { name: 'Ultrafarma', image: 'assets/pharmacies/ultrafarma_logo.jpg', distance: '1.7 km', time: '30-40 min', tax: '4,99' },
+    { name: 'Pague Menos', image: 'assets/pharmacies/pague_menos_logo.jpg', distance: '2.8 km', time: '45-55 min', tax: '6,99' },
+    { name: 'Droga Raia', image: 'assets/pharmacies/droga_raia_logo.jpg', distance: '4.9 km', time: '70-50 min', tax: '11,99' }
 ];
