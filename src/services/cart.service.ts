a560b02fb93c0081c4e975138e1d6ce9a4f1a6dc
Humberto <humbertoodantas@gmail.com>
2022-04-04 00:30:38 -0300

    Remove cart item and calc total cart

--- src/services/cart.service.ts
+++ src/services/cart.service.ts
@@ -11,6 +11,10 @@ export class CartService {
       this.items.push(product);
   }
 
+  removeFromCart(product: Product) {
+    this.items.splice(this.items.indexOf(product), 1);
+  }
+
   getItems() {
       return this.items;
   }
@@ -18,5 +22,16 @@ export class CartService {
   clearCart() {
       this.items = [];
       return this.items;
+  }
+
+  cartTotal() {
+    let price = 0;
+    for(let i = 0; i < this.items.length; i++) {
+      let rep1 = this.items[i]['price'].replace("R$ ", "");
+      let rep2 = rep1.replace(",", ".");
+
+      price = price + parseFloat(rep2);
+    }
+    return price.toFixed(2);
   }
 }
