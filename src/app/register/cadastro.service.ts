2cb7b2a0810fcd8f44ce4b6eacc3a1f78c0a7261
Humberto <humbertoodantas@gmail.com>
2022-04-04 00:39:11 -0300

    Fild validations

--- src/app/register/cadastro.service.ts
+++ src/app/register/cadastro.service.ts
@@ -15,6 +15,53 @@ export class CadastroService {
   constructor(private router: Router, private http: HttpClient) { }
 
   async makeLogin(register: Register) {
+    const nameField = (<HTMLSelectElement>document.getElementById('name'));
+    const emailField = (<HTMLSelectElement>document.getElementById('email'));
+    const passField = (<HTMLSelectElement>document.getElementById('pass'));
+    const numberField = (<HTMLSelectElement>document.getElementById('number'));
+    const complementField = (<HTMLSelectElement>document.getElementById('complement'));
+    const zipCodeField = (<HTMLSelectElement>document.getElementById('zipcode'));
+    const cpfField = (<HTMLSelectElement>document.getElementById('cpf'));
+    const birthDateField = (<HTMLSelectElement>document.getElementById('birthdate'));
+    const sexField = (<HTMLSelectElement>document.getElementById('sex'));
+    const sendBtn = (<HTMLSelectElement>document.getElementById('sendBtn'));
+
+    nameField.disabled = true;
+    emailField.disabled = true;
+    passField.disabled = true;
+    numberField.disabled = true;
+    complementField.disabled = true;
+    zipCodeField.disabled = true;
+    cpfField.disabled = true;
+    birthDateField.disabled = true;
+    sexField.disabled = true;
+    sendBtn.disabled = true;
+
+    const notfication = (<HTMLSelectElement>document.getElementById('div-not'));
+    const notficationText = (<HTMLSelectElement>document.getElementById('notification'));
+
+    var complement2 = "";
+    if(register.complement == undefined || register.complement == "") {
+      complement2 = "NULL";
+    }
+    else {
+      if(register.complement.length >= 50) {
+        notficationText.innerText="COMPLEMENTO MUITO GRANDE";
+        notfication.classList.remove("hide-div-not");
+        return;
+      }
+  
+      var re = /^[0-9A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÊÍÏÓÔÕÖÚÇÑ . - \\s]+?$/i;
+      let complementRegex = re.test(register.complement);
+      if(!complementRegex) {
+        notficationText.innerText="COMPLEMENTO INVÁLIDO";
+        notfication.classList.remove("hide-div-not");
+        return;
+      }
+
+      complement2 = register.complement;
+    }
+
     const url = 'https://pharmacy-delivery.herokuapp.com/client/signup';
     const json = JSON.stringify({
       name:register.name,
@@ -22,7 +69,7 @@ export class CadastroService {
       pass:register.pass,
       street:register.street,
       number:register.number,
-      complement:register.complement,
+      complement:complement2,
       zipCode:register.zipCode,
       state:register.state,
       city:register.city,
@@ -41,6 +88,21 @@ export class CadastroService {
       if(xhttp.status == 201) {
         window.location.replace("/login");
       }
+      else {
+        notficationText.innerText="OCORREU UM ERRO";
+        notfication.classList.remove("hide-div-not");
+
+        nameField.disabled = false;
+        emailField.disabled = false;
+        passField.disabled = false;
+        numberField.disabled = false;
+        complementField.disabled = false;
+        zipCodeField.disabled = false;
+        cpfField.disabled = false;
+        birthDateField.disabled = false;
+        sexField.disabled = false;
+        sendBtn.disabled = false;
+      }
     });
   }
 
@@ -55,9 +117,8 @@ export class CadastroService {
 
     xhttp.addEventListener('loadend', () => {
       var endereco = JSON.parse(xhttp.response);
-
+      cepFild.disabled = false;
       if(endereco['cep'] != undefined) {
-        cepFild.disabled = false;
 
         var newCep = endereco['cep'];
         var newRua = endereco['logradouro'];
@@ -94,13 +155,160 @@ export class CadastroService {
       }
     } 
 
-    if(idade > 18) {
-      return false;
-    }
-    else {
+    if(idade >= 18) {
       return true;
     }
+    else {
+      return false;
+    }
     
+  }
+
+  async checkData(register: Register) {
+    const notfication = (<HTMLSelectElement>document.getElementById('div-not'));
+    const notficationText = (<HTMLSelectElement>document.getElementById('notification'));
+    notfication.classList.add("hide-div-not");
+
+    if(register.email == undefined || register.email == "" ||
+      register.pass == undefined || register.pass == "" ||
+      register.name == undefined || register.name == "" ||
+      register.cpf == undefined || register.cpf == "" ||
+      register.street == undefined || register.street == "" ||
+      register.state == undefined || register.state == "" ||
+      register.zipCode == undefined || register.zipCode == "" ||
+      register.city == undefined || register.city == "" ||
+      register.district == undefined || register.district == "" ||
+      register.birthDate == undefined || register.birthDate == "" ||
+      register.number == undefined || register.number == "") {
+      notficationText.innerText="PREENCHA TODOS OS CAMPOS";
+      notfication.classList.remove("hide-div-not");
+      return false;
+    }
+
+    if(register.email.length <= 10) {
+      notficationText.innerText="E-MAIL MUITO PEQUENO";
+      notfication.classList.remove("hide-div-not");
+      return false;
+    }
+
+    if(register.email.length >= 60) {
+      notficationText.innerText="E-MAIL MUITO GRANDE";
+      notfication.classList.remove("hide-div-not");
+      return false;
+    }
+
+    var re = /^[A-Za-z0-9-_.]+@[A-Za-z0-9]+\.[A-Za-z .]+?$/i;
+    let emailRegex = re.test(register.email);
+    if(!emailRegex) {
+      notficationText.innerText="E-MAIL INVÁLIDO";
+      notfication.classList.remove("hide-div-not");
+      return false;
+    }
+
+    if(register.pass.length < 8) {
+      notficationText.innerText="SENHA MUITO PEQUENA";
+      notfication.classList.remove("hide-div-not");
+      return false;
+    }
+
+    if(register.pass.length >= 25) {
+      notficationText.innerText="SENHA MUITO GRANDE";
+      notfication.classList.remove("hide-div-not");
+      return false;
+    }
+
+    var re = /^[A-Za-z0-9-_.!@#]+?$/i;
+    let passRegex = re.test(register.pass);
+    if(!passRegex) {
+      notficationText.innerText="SENHA INVÁLIDA";
+      notfication.classList.remove("hide-div-not");
+      return false;
+    }
+
+    if(this.calcIdade(register.birthDate)['__zone_symbol__value'] == false) {
+      notficationText.innerText="É NECESSÁRIO TER A IDADE MÍNIMA DE 18 ANOS";
+      notfication.classList.remove("hide-div-not");
+      return;
+    }
+
+    if(register.number.length >= 10) {
+      notficationText.innerText="NÚMERO MUITO GRANDE";
+      notfication.classList.remove("hide-div-not");
+      return false;
+    }
+
+    if(register.number.length < 1) {
+      notficationText.innerText="NÚMERO MUITO PEQUENO";
+      notfication.classList.remove("hide-div-not");
+      return false;
+    }
+
+    var re = /^[0-9]+?$/i;
+    let numberRegex = re.test(register.number);
+    if(!numberRegex) {
+      notficationText.innerText="NÚMERO INVÁLIDO";
+      notfication.classList.remove("hide-div-not");
+      return false;
+    }
+
+    if(register.name.length >= 50) {
+      notficationText.innerText="NOME MUITO GRANDE";
+      notfication.classList.remove("hide-div-not");
+      return false;
+    }
+
+    let testName = register.name.split(" ");
+    if(testName.length < 2) {
+      notficationText.innerText="INSIRA SEU SOBRENOME";
+      notfication.classList.remove("hide-div-not");
+      return false;
+    }
+
+    var re = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÊÍÏÓÔÕÖÚÇÑ \\s]+?$/i;
+    let nameRegex = re.test(register.name);
+    if(!nameRegex) {
+      notficationText.innerText="NOME INVÁLIDO";
+      notfication.classList.remove("hide-div-not");
+      return false;
+    }
+
+    if(register.cpf.length >= 15) {
+      notficationText.innerText="CPF MUITO GRANDE";
+      notfication.classList.remove("hide-div-not");
+      return false;
+    }
+
+    if(register.cpf.length < 11) {
+      notficationText.innerText="CPF MUITO PEQUENO";
+      notfication.classList.remove("hide-div-not");
+      return false;
+    }
+
+    if(register.cpf.length >= 12) {
+      if(register.cpf.length <= 13) {
+        notficationText.innerText="CPF INVÁLIDO";
+        notfication.classList.remove("hide-div-not");
+        return false;
+      }
+    }
+
+    var re = /^[0-9]+?$/i;
+    let cpf1Regex = re.test(register.cpf);
+    if(register.cpf.length == 11 && !cpf1Regex) {
+      notficationText.innerText="CPF INVÁLIDO";
+      notfication.classList.remove("hide-div-not");
+      return false;
+    }
+
+    var re = /^[0-9]{3}[.][0-9]{3}[.][0-9]{3}[-][0-9]{2}?$/i;
+    let cpf2Regex = re.test(register.cpf);
+    if(register.cpf.length == 14 && !cpf2Regex) {
+      notficationText.innerText="CPF INVÁLIDO";
+      notfication.classList.remove("hide-div-not");
+      return false;
+    }
+
+    return true;
   }
 
   get userAuthenticated(): boolean {
