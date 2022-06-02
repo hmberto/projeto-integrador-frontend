f6165d30876a49bec61827232d729d0673a612f3
Humberto <humbertoodantas@gmail.com>
2022-06-02 02:33:43 -0300

    Order from DB

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,8 +1,7 @@
 import { Router } from '@angular/router';
-
 import { Component, OnInit } from '@angular/core';
-
 import { OrderService } from './order.service';
+import { Order } from '../../models/order';
 
 @Component({
   selector: 'app-order',
@@ -10,12 +9,36 @@ import { OrderService } from './order.service';
   styleUrls: ['./order.component.css']
 })
 export class OrderComponent implements OnInit {
-  constructor(private OrderService: OrderService,
+  constructor(private orderService: OrderService,
     private router: Router) { }
 
   newPrice = [];
 
-  ngOnInit() {}
+  ngOnInit() {
+    const session = window.localStorage.getItem("session");
+    const urlParams = new URLSearchParams(window.location.search);
+    const myParam = urlParams.get('orderId');
+    if(session == null || session == "null") {
+      this.router.navigate(['login'], { queryParams: { checkout: 'true' } });
+    }
+    if(myParam == null || myParam == "null") {
+      this.router.navigate(['']);
+    }
+    
+    this.orderData(myParam, session);
+  }
+
+  userTime() {
+    this.orderService.userTime("2022-06-01 22:34:43", 15);
+  }
+
+  orderData(orderId, session) {
+    this.orderService.orderData(orderId, session);
+  }
+
+  get order(): Order[] {
+    return this.orderService.gettedOrder;
+  }
 
   get orderNumber() {
     this.newPrice = [];
